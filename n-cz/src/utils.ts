75c39ed5e011c0d3c6246bf854fe9dffb1a65e29
mengpeng <pengmeng@yangqianguan.com>
2021-05-20 20:21:35 +0800

    [feat][two] 测试

--- n-cz/src/utils.ts
+++ n-cz/src/utils.ts
@@ -56,7 +56,6 @@ export const setCommit = (options: Option) => {
             message: '本次修改的概要信息',
         },
     ];
-    console.log(isInput,promptOption[1],choices)
     return {
         prompter: (cz: any, commit: any) => {
             cz.prompt(promptOption).then((answer: Answer) => {
